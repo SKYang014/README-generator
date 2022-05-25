16f9e39dfcffc594ed4af390db4700d8f6b1840c
poofy <skyang014@gmail.com>
2022-05-24 20:51:42 -0700

    added questiosn

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,112 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+//Creates the file/project name
+const promptUser = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: '# ',
+        message: 'What is your Project name? (Required)',
+        validate: projectNameInput => {
+        if (projectNameInput) {
+            return true;
+        } else {
+            console.log('Please enter your Project name!');
+            return false;
+        }
+        }
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your Github username? (Required)',
+            validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your username!');
+                return false;
+            }
+            }
+        },
+        {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+        type: 'confirm',
+        name: 'confirmInstallation',
+        message: 'Would you like to enter installation instructions?',
+        default: true
+        },
+        {
+        type: 'input',
+        name: 'install',
+        message: 'Provide installation instructions:',
+        when: ({ confirmInstallation }) => confirmInstallation
+        },
+        {
+            type: 'confirm',
+            name: 'confirmUsage',
+            message: 'Would you like to enter usage information?',
+            default: true
+            },
+            {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide some information about the usage of your project:',
+            when: ({ confirmUsage }) => confirmUsage
+            },
+            {
+                type: 'confirm',
+                name: 'confirmContrib',
+                message: 'Would you like to enter some information about contribution guidelines?',
+                default: true
+                },
+                {
+                type: 'input',
+                name: 'contrib',
+                message: 'Provide some information about contribution guidelines:',
+                when: ({ confirmContrib }) => confirmContrib
+                },
+                {
+                    type: 'confirm',
+                    name: 'confirmTest',
+                    message: 'Would you like to enter some information about test instructions?',
+                    default: true
+                    },
+                    {
+                    type: 'input',
+                    name: 'test',
+                    message: 'Provide some information about test instructions:',
+                    when: ({ confirmTest }) => confirmTest
+                    },
+        
+                
+    ])
+    .then((answers) => {
+        const filename = ("README.ME");
+    
+    fs.writeFile(filename, JSON.stringify(answers, null, '\t'), err =>
+    err ? console.log(err) : console.log('success!')
+    );
+    
+    });
+}
+
+promptUser()
